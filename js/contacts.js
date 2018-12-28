ca807b6fdecebb48614b96d9d738886876fae41b
Maxim Bondarenko <maximbndrnk@gmail.com>
2018-12-28 10:52:43 +0200

    update contacts.html, show contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,35 +1,38 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('Contacts');
-    // getContactsFromJSON();
+document.addEventListener('DOMContentLoaded', function () {
+    getContactsFromJSON();
 });
 
 
 function getContactsFromJSON() {
     var tableContainer = document.querySelector('#table-container');
+    var table = document.querySelector('#table-contacts');
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'php/getContacts.php', true);
 
     xhr.onload = function () {
         if (this.status == 200) {
+            console.log('this.responseText!'+this.responseText.length+'!');
+            console.log('this.responseText!',this.responseText);
+            if(!this.responseText.length){
+                tableContainer.innerHTML = '<p class="no-records">No records yet</p>';
+            }
             var lists = JSON.parse(this.responseText);
-            
-            var output = '';
+
+            var output = '<tr>' +
+                '<th class="th-name">Name</th><th class="th-contact">Contact Data</th>\n' +
+                '</tr>';
             for (var i in lists) {
-                output += '<div class="one-shopping-list" onclick="showList('+ 
-                 lists[i].id+')"><p class="list-name">'
-                    + lists[i].name +
-                    '</p><p class="list-created">Added:'+ lists[i].createDate +'</p><p class="list-created">Updated:'+ lists[i].updateDate +'</p></div>';
+                output += '<tr><td  class="th-name">'
+                    + lists[i].name +'</td><td class="th-contact">'+lists[i].contact + '</td>';
             }
-            if(output == ''){
-                output += '<div class="one-shopping-list"><p class="list-name">'
-                    + 'No records' +
-                    '</p></div>';
+            if (output == '') {
+                output += '<tr><th>No records</th><th>No records</th></tr>';
             }
-            tableContainer.innerHTML = output;
+            table.innerHTML = output;
         }
-    }
+    };
 
     xhr.send();
 }
